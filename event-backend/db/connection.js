c93a40930b10d54af7f78f45eebf39ba62ebeaa0
navami21 <navamib2023@gmail.com>
2025-06-10 22:55:07 +0530

    updated

--- event-backend/db/connection.js
+++ event-backend/db/connection.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose')
 require('dotenv').config();
 
-mongoose.connect(process.env.MongoDB_URL).then(()=>{
+mongoose.connect(process.env.mongoDB_URL).then(()=>{
     console.log('Connection is established')
 }).catch(()=>{
     console.log('Connection error')
